46f404ad9d1134fab41680fadd863558f2214e75
Meegan1 <meegan118@gmail.com>
2024-08-10 02:43:25 +0100

    fix: updated lualine config to include copilot + searchcount

---
+++ lua/config/lualine/local.lua
@@ -0,0 +1,2 @@
+vim.o.laststatus = 3
+vim.opt.cmdheight = 0
--- lua/plugins/local/lualine.lua
+++ lua/plugins/local/lualine.lua
@@ -1,7 +1,29 @@
 return {
   {
+    'AndreM222/copilot-lualine',
+  },
+  {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = {
+      'AndreM222/copilot-lualine',
+      'nvim-tree/nvim-web-devicons',
+    },
     config = true,
-  }
+    opts = {
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff',
+          {
+            'diagnostics',
+            sources = { "nvim_diagnostic" },
+            symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+          }
+        },
+        lualine_c = { 'filename' },
+        lualine_x = { 'copilot', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location', 'searchcount' },
+      },
+    }
+  },
 }
