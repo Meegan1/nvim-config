46f0c6c11951af7974a61c685cc7fbf30076a872
Meegan1 <meegan118@gmail.com>
2024-08-16 09:46:03 +0100

    feat: add cmd+p telescope find_files shortcut

--- lua/plugins/local/init.lua
+++ lua/plugins/local/init.lua
@@ -1,7 +1,16 @@
 return {
   { "tpope/vim-commentary", },
   { 'markonm/traces.vim' },
-  { 'nvim-telescope/telescope.nvim' },
+  {
+    'nvim-telescope/telescope.nvim',
+    config = function(_, opts)
+      require('telescope').setup(opts)
+
+      vim.keymap.set({ 'n', 'i', 'v' }, '<D-p>', function()
+        require('telescope.builtin').find_files()
+      end)
+    end
+  },
   {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
