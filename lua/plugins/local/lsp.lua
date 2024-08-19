fbec2a839a4f575e74ec55324e1479a4016291b1
Meegan1 <meegan118@gmail.com>
2024-08-19 10:13:40 +0100

    feat: add prettier config

--- lua/plugins/local/lsp.lua
+++ lua/plugins/local/lsp.lua
@@ -1,13 +1,55 @@
 return {
   {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     lazy = false,
     config = true,
   },
-  { 'williamboman/mason-lspconfig.nvim' },
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    "nvimtools/none-ls.nvim",
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          "stylua",
+          "prettierd",
+        },
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      },
+      -- Set default options
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 1000 },
+    },
+    ---@param opts ConformOpts
+    config = function(_, opts)
+      require("conform").setup(opts)
+    end,
+  },
+  { "williamboman/mason-lspconfig.nvim" },
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
     lazy = true,
     config = false,
     init = function()
@@ -17,19 +59,19 @@ return {
     end,
   },
   {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
+    "neovim/nvim-lspconfig",
+    cmd = { "LspInfo", "LspInstall", "LspStart" },
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'williamboman/mason-lspconfig.nvim' },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "williamboman/mason-lspconfig.nvim" },
     },
     config = function()
       -- This is where all the LSP shenanigans will live
-      local lsp_zero = require('lsp-zero')
+      local lsp_zero = require("lsp-zero")
       lsp_zero.extend_lspconfig()
 
-      local util = require('vim.lsp.util')
+      local util = require("vim.lsp.util")
 
       local Config = require("noice.config")
       local Docs = require("noice.lsp.docs")
@@ -44,11 +86,11 @@ return {
         lsp_zero.buffer_autoformat()
 
         -- bind gh to vim.lsp.buf.hover()
-        vim.keymap.set('n', 'gh', function()
+        vim.keymap.set("n", "gh", function()
           -- vim.lsp.buf.hover()
 
           local params = util.make_position_params()
-          vim.lsp.buf_request(0, 'textDocument/hover', params, function(err, result, ctx)
+          vim.lsp.buf_request(0, "textDocument/hover", params, function(err, result, ctx)
             local buf = vim.api.nvim_get_current_buf()
             -- local pos_info = vim.inspect_pos(vim.api.nvim_get_current_buf(), mouse_pos.line - 1, mouse_pos.column - 1)
 
@@ -59,7 +101,6 @@ return {
               kind = "markdown",
               value = result and result.contents and result.contents.value or "",
             }
-
 
             -- if diags length > 0 then prepend Diagnostics:
             if #diags > 0 then
@@ -73,12 +114,19 @@ return {
             for i, diag in ipairs(diags) do
               print(diag.message)
 
-              contents.value = contents.value .. "\n" .. i .. ". " .. diag.message .. " \\[" .. diag.code .. "\\]"
+              contents.value = contents.value
+                  .. "\n"
+                  .. i
+                  .. ". "
+                  .. diag.message
+                  .. " \\["
+                  .. diag.code
+                  .. "\\]"
             end
 
             print(contents.value)
 
-            if not (contents) then
+            if not contents then
               if Config.options.lsp.hover.silent ~= true then
                 vim.notify("No information available")
               end
@@ -97,21 +145,20 @@ return {
               end
               Docs.show(message)
             end
-          end
-          )
+          end)
         end, { buffer = bufnr, desc = "Show hover doc" })
 
         -- bind gH to vim.diagnostic.open_float()
-        vim.keymap.set('n', 'gH', function()
+        vim.keymap.set("n", "gH", function()
           vim.diagnostic.open_float(nil, {
-            anchor_bias = 'below',
+            anchor_bias = "below",
             focusable = true,
             -- relative = "mouse"
           })
         end, { buffer = bufnr, desc = "Show signature help" })
 
         -- bind <Esc> to close the hover buffer
-        vim.keymap.set('n', '<Esc>', function()
+        vim.keymap.set("n", "<Esc>", function()
           for _, win in ipairs(vim.api.nvim_list_wins()) do
             local config = vim.api.nvim_win_get_config(win)
             if config.relative ~= "" then
@@ -121,25 +168,36 @@ return {
         end, { buffer = bufnr, desc = "Close hover doc" })
       end)
 
-
-      require('mason-lspconfig').setup({
+      require("mason-lspconfig").setup({
         -- LSP servers to ensure are installed
         ensure_installed = {
           "lua_ls",
           "tsserver",
           "intelephense",
+          "tailwindcss",
         },
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
           function(server_name)
-            require('lspconfig')[server_name].setup({})
+            require("lspconfig")[server_name].setup({})
           end,
-        }
+        },
       })
-    end
+    end,
   },
-  { 'hrsh7th/cmp-nvim-lsp' },
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig",      -- optional
+    },
+    opts = {},                      -- your configuration
+  },
+  { "hrsh7th/cmp-nvim-lsp" },
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
@@ -152,54 +210,55 @@ return {
   },
   { "Bilal2453/luvit-meta", lazy = true },
   {
-    'hrsh7th/nvim-cmp',
+    "onsails/lspkind.nvim",
+  },
+  {
+    "hrsh7th/nvim-cmp",
     version = false, -- last release is way too old
-    event = 'InsertEnter',
+    event = "InsertEnter",
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'L3MON4D3/LuaSnip',
-      'windwp/nvim-autopairs',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "windwp/nvim-autopairs",
+      "luckasRanarison/tailwind-tools.nvim",
     },
     config = function()
       -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
+      local lsp_zero = require("lsp-zero")
       lsp_zero.extend_cmp()
 
       -- And you can configure cmp even more, if you want to.
-      local cmp = require('cmp')
+      local cmp = require("cmp")
       local cmp_action = lsp_zero.cmp_action()
 
       -- Autopairs when selecting a autocompletion
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      cmp.event:on(
-        'confirm_done',
-        cmp_autopairs.on_confirm_done()
-      )
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
       cmp.setup({
         auto_brackets = {}, -- configure any filetype to auto add brackets
         completion = {
-          completeopt = "menu,menuone,noinsert"
+          completeopt = "menu,menuone,noinsert",
         },
         -- formatting = lsp_zero.cmp_format({ details = true }),
         mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<S-Esc>'] = cmp.mapping.close(),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-Esc>"] = cmp.mapping.close(),
 
-          ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-          ['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-          ['<C-l>'] = cmp.mapping.confirm({ select = true }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+          ["<C-l>"] = cmp.mapping.confirm({ select = true }),
         }),
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         sources = cmp.config.sources({
@@ -209,7 +268,7 @@ return {
           {
             name = "lazydev",
             group_index = 0,
-          }
+          },
         }),
         window = {
           completion = cmp.config.window.bordered(),
@@ -218,18 +277,23 @@ return {
         -- here is where the change happens
         format = function(entry, item)
           local menu_icon = {
-            nvim_lsp = 'λ',
-            luasnip = '⋗',
-            buffer = 'Ω',
-            path = '🖫',
-            nvim_lua = 'Π',
+            nvim_lsp = "λ",
+            luasnip = "⋗",
+            buffer = "Ω",
+            path = "🖫",
+            nvim_lua = "Π",
           }
 
           item.menu = menu_icon[entry.source.name]
           return item
         end,
+        formatting = {
+          format = require("lspkind").cmp_format({
+            before = require("tailwind-tools.cmp").lspkind_format,
+          }),
+        },
       })
-    end
+    end,
   },
-  { 'L3MON4D3/LuaSnip' },
+  { "L3MON4D3/LuaSnip" },
 }
