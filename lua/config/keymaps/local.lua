b1e6aab35994964e3f6aaba7c4975beddb97607a
Meegan1 <meegan118@gmail.com>
2024-08-27 10:45:04 +0100

    chore: format file

--- lua/config/keymaps/local.lua
+++ lua/config/keymaps/local.lua
@@ -1,42 +1,57 @@
 -- map BufferNext and BufferPrevious to gt and gT
-vim.keymap.set('n', 'gt', function()
-  vim.cmd('BufferNext')
+vim.keymap.set("n", "gt", function()
+  vim.cmd("BufferNext")
 end)
-vim.keymap.set('n', 'gT', function()
-  vim.cmd('BufferPrevious')
+vim.keymap.set("n", "gT", function()
+  vim.cmd("BufferPrevious")
 end)
 
 -- map shift + tab to indent left
-vim.keymap.set('i', '<S-Tab>', '<C-d>')
+vim.keymap.set("i", "<S-Tab>", "<C-d>")
 
 -- save on cmd + s
-vim.keymap.set({ 'n', 'i', 'v' }, '<D-s>', function()
-  vim.cmd('w')
+vim.keymap.set({ "n", "i", "v" }, "<D-s>", function()
+  vim.cmd("w")
 end)
 
-vim.keymap.set({ 'n', 'i', 'v' }, '<D-w>', function()
-  vim.cmd('CloseTab')
+vim.keymap.set({ "n", "i", "v" }, "<D-w>", function()
+  vim.cmd("CloseTab")
 end)
 
 -- reopen last closed buffer on cmd + shift + t
-vim.keymap.set({ 'n', 'i', 'v' }, '<S-D-T>', function()
-  vim.cmd('BufferRestore')
+vim.keymap.set({ "n", "i", "v" }, "<S-D-T>", function()
+  vim.cmd("BufferRestore")
 end)
 
 -- create a new buffer on cmd + n
-vim.keymap.set({ 'n' }, '<D-n>', function()
-  vim.cmd('enew')
+vim.keymap.set({ "n" }, "<D-n>", function()
+  vim.cmd("enew")
 end)
 
 -- undo on cmd + z
-vim.keymap.set({ 'n', 'i' }, '<D-z>', function()
-  vim.cmd('u')
+vim.keymap.set({ "n", "i" }, "<D-z>", function()
+  vim.cmd("u")
 end)
 
 -- redo on cmd + shift + z
-vim.keymap.set({ 'n', 'i' }, '<S-D-Z>', function()
-  vim.cmd('redo')
+vim.keymap.set({ "n", "i" }, "<S-D-Z>", function()
+  vim.cmd("redo")
 end)
 
 -- escape from terminal mode with shift + escape
-vim.keymap.set('t', '<S-Esc>', '<C-\\><C-n>')
+vim.keymap.set("t", "<S-Esc>", "<C-\\><C-n>")
+
+-- -- copy to clipboard
+-- vim.keymap.set({ "n", "v" }, "<D-c>", function()
+--   vim.cmd("silent! !pbcopy")
+-- end)
+
+-- -- paste from clipboard
+-- vim.keymap.set({ "n", "i", "v" }, "<D-v>", function()
+--   vim.cmd("silent! !pbpaste")
+-- end)
+
+-- lsp rename
+vim.keymap.set("n", "<leader>rn", function()
+  vim.lsp.buf.rename()
+end)
