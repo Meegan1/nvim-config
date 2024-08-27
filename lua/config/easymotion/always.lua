6ff1ff62383c1ad6e74c2b5f637393360171b251
Meegan1 <meegan118@gmail.com>
2024-08-27 10:44:46 +0100

    fix: easymotion breaking on local from conflict with vscode config

--- lua/config/easymotion/always.lua
+++
@@ -1,3 +0,0 @@
--- <Leader>f{char} to move to {char}
-vim.api.nvim_set_keymap('n', '<Leader>f', '<Plug>(easymotion-bd-f)', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>f', '<Plug>(easymotion-overwin-f)', { noremap = true, silent = true })
---
+++ lua/config/easymotion/vscode.lua
@@ -0,0 +1,2 @@
+-- <Leader>f{char} to move to {char}
+vim.api.nvim_set_keymap("n", "<Leader>f", "<Plug>(easymotion-overwin-f)", { noremap = true, silent = true })
