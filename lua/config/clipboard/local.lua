e95ad7eaf2d7354d2eadf9a374154e04f2fae950
Meegan1 <meegan118@gmail.com>
2024-08-28 10:38:54 +0100

    feat: enable osc 52 clipboard

---
+++ lua/config/clipboard/local.lua
@@ -0,0 +1,12 @@
+-- Use OSC 52 clipboard for neovim
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
