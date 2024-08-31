ff7d4dd0b19c07c3fac6ae7b426c6a89efbc7c77
Meegan1 <meegan118@gmail.com>
2024-08-31 02:33:34 +0100

    feat: add OSC 52 clipboard support for use in ssh environment

--- lua/config/clipboard/local.lua
+++ lua/config/clipboard/local.lua
@@ -1,12 +1,31 @@
+local function my_paste(reg)
+	local content
+
+	-- get os
+	local os = vim.uv.os_uname().sysname
+	if os == "Linux" then
+		content = "xclip -o -selection clipboard"
+	elseif os == "Darwin" then
+		content = "pbpaste"
+	elseif os == "Windows" then
+		content = "Get-Clipboard"
+	else
+		print("Unsupported OS")
+		return
+	end
+
+	return content
+end
+
 -- Use OSC 52 clipboard for neovim
 vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = my_paste("+"),
+		["*"] = my_paste("*"),
+	},
 }
