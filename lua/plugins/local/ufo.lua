d607164f51a6c701abf42542388b7fd103f3ba0b
Meegan1 <meegan118@gmail.com>
2024-08-31 02:32:13 +0100

    fix: use opt instead of o and set foldcolumn to 0

--- lua/plugins/local/ufo.lua
+++ lua/plugins/local/ufo.lua
@@ -9,10 +9,10 @@ return {
       "neovim/nvim-lspconfig",
     },
     config = function()
-      vim.o.foldcolumn = "1"
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
+      vim.opt.foldcolumn = "0"
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+      vim.opt.foldenable = true
 
       vim.keymap.set("n", "zO", require("ufo").openAllFolds)
       vim.keymap.set("n", "zC", require("ufo").closeAllFolds)
