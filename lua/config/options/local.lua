98322d8cfc6fdc6aa5a01bb0ad97cae75ebb8a0a
Meegan1 <meegan118@gmail.com>
2024-09-01 16:33:21 +0100

    feat: enable spell check

--- lua/config/options/local.lua
+++ lua/config/options/local.lua
@@ -11,3 +11,7 @@ vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpo
 vim.opt.cursorline = true
 vim.opt.wrap = false
 vim.opt.undofile = true
+
+-- spell check
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
