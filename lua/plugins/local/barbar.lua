dbb4ec36b994e4054216b226c3f5d874c01b61c9
Meegan1 <meegan118@gmail.com>
2024-09-02 00:12:05 +0100

    feat: add buffer picker shortcut

--- lua/plugins/local/barbar.lua
+++ lua/plugins/local/barbar.lua
@@ -1,49 +1,55 @@
 return {
-  {
-    "romgrk/barbar.nvim",
-    event = "BufRead",
-    opts = {
-      sidebar_filetypes = {
-        ["neo-tree"] = true,
-      },
-    },
-    dependencies = {
-      {
-        "kyazdani42/nvim-web-devicons",
-      },
-      {
-        "lewis6991/gitsigns.nvim",
-      },
-    },
-    keys = {
-      {
-        "<A-b>h",
-        function()
-          vim.cmd("BufferMovePrevious")
-        end,
-        desc = "Move buffer left",
-      },
-      {
-        "<A-b>l",
-        function()
-          vim.cmd("BufferMoveNext")
-        end,
-        desc = "Move buffer right",
-      },
-      {
-        "<A-b>d",
-        function()
-          vim.cmd("BufferClose")
-        end,
-        desc = "Close buffer",
-      },
-      {
-        "<A-b>D",
-        function()
-          vim.cmd("BufferCloseAllButVisible")
-        end,
-        desc = "Close all buffers but visible",
-      },
-    },
-  },
+	{
+		"romgrk/barbar.nvim",
+		event = "BufRead",
+		opts = {
+			sidebar_filetypes = {
+				["neo-tree"] = true,
+			},
+		},
+		dependencies = {
+			{
+				"kyazdani42/nvim-web-devicons",
+			},
+			{
+				"lewis6991/gitsigns.nvim",
+			},
+		},
+		keys = {
+			{
+				"<A-b>h",
+				function()
+					vim.cmd("BufferMovePrevious")
+				end,
+				desc = "Move buffer left",
+			},
+			{
+				"<A-b>l",
+				function()
+					vim.cmd("BufferMoveNext")
+				end,
+				desc = "Move buffer right",
+			},
+			{
+				"<A-b>d",
+				function()
+					vim.cmd("BufferClose")
+				end,
+				desc = "Close buffer",
+			},
+			{
+				"<A-b>D",
+				function()
+					vim.cmd("BufferCloseAllButVisible")
+				end,
+				desc = "Close all buffers but visible",
+			},
+			{
+				"<leader>s",
+				function()
+					vim.cmd("BufferPick")
+				end,
+			},
+		},
+	},
 }
