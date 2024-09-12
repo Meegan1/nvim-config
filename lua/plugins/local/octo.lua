a928afeb90c32d1f91836c7003818a12fcd3e505
Meegan1 <meegan118@gmail.com>
2024-09-12 16:37:26 +0100

    feat: add CheckoutIssue command for using gh issue develop to checkout a github issue

--- lua/plugins/local/octo.lua
+++ lua/plugins/local/octo.lua
@@ -9,6 +9,32 @@ return {
     },
     config = function()
       require("octo").setup()
+      local Job = require("plenary.job")
+
+      -- add a command for checking out an issue from issue id in popup
+      vim.api.nvim_create_user_command("CheckoutIssue", function()
+        -- get the issue id from popup
+        local issue_id = vim.fn.input("Issue ID: ")
+
+        -- check if issue id is not empty
+        if issue_id ~= "" then
+          -- run gh issue develop <issue_id> --checkout
+          Job:new({
+            command = "gh",
+            args = { "issue", "develop", issue_id, "--checkout" },
+            on_exit = function(j, return_val)
+              if return_val == 0 then
+                vim.notify("Checked out issue", "info", { title = "Octo" })
+              else
+                vim.notify(vim.inspect(j:stderr_result()), "error", { title = "Octo" })
+                vim.notify("Failed to checkout issue", "error", { title = "Octo" })
+              end
+            end,
+          }):start()
+        end
+      end, {
+        desc = "Checkout an issue",
+      })
     end,
     cond = function()
       -- check if gh is installed
