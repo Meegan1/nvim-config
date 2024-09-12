6bd8921fe35b2b75d524b747a2800037e628731b
Meegan1 <meegan118@gmail.com>
2024-09-12 13:06:15 +0100

    fix: clipboard support inside ssh/devcontianer

--- lua/config/clipboard/local.lua
+++ lua/config/clipboard/local.lua
@@ -1,31 +1,46 @@
+-- vim.o.clipboard = "unnamedplus"
+
 local function my_paste(reg)
-	local content
+  -- return function(lines)
+  --   local content = vim.fn.getreg('"')
+  --   return vim.split(content, "\n")
+  -- end
 
-	-- get os
-	local os = vim.uv.os_uname().sysname
-	if os == "Linux" then
-		content = "xclip -o -selection clipboard"
-	elseif os == "Darwin" then
-		content = "pbpaste"
-	elseif os == "Windows" then
-		content = "Get-Clipboard"
-	else
-		print("Unsupported OS")
-		return
-	end
+  -- local content
+  -- get os
+  local os = vim.uv.os_uname().sysname
+  if os == "Linux" then
+    -- check if xclip is installed
+    if vim.fn.executable("xclip") == 1 then
+      content = "xclip -o -selection clipboard"
+    else
+      -- info: disable inside wezterm, as wezterm doesn't support reading from OSC 52
+      return function(lines)
+        local content = vim.fn.getreg('"')
+        return vim.split(content, "\n")
+      end
+    end
+  elseif os == "Darwin" then
+    content = "pbpaste"
+  elseif os == "Windows" then
+    content = "Get-Clipboard"
+  else
+    print("Unsupported OS")
+    return
+  end
 
-	return content
+  return content
 end
 
 -- Use OSC 52 clipboard for neovim
 vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = my_paste("+"),
-		["*"] = my_paste("*"),
-	},
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = my_paste("+"),
+    ["*"] = my_paste("*"),
+  },
 }
