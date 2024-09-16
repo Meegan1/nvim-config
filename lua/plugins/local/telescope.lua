6628005074df9d714538cbbf2e54cb2c811ff417
Meegan1 <meegan118@gmail.com>
2024-09-16 10:09:43 +0100

    feat: update keymaps for telescope undo

--- lua/plugins/local/telescope.lua
+++ lua/plugins/local/telescope.lua
@@ -1,65 +1,67 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = function()
-      return {
-        defaults = {
-          mappings = {
-            n = {
-              ["l"] = require("telescope.actions").select_default,
-              ["<D-.>"] = require("telescope.actions").select_default,
-            },
-          },
-        },
-      }
-    end,
-    config = function(_, opts)
-      require("telescope").setup(opts)
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = function()
+			return {
+				defaults = {
+					mappings = {
+						n = {
+							["l"] = require("telescope.actions").select_default,
+							["<D-.>"] = require("telescope.actions").select_default,
+						},
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("telescope").setup(opts)
 
-      vim.keymap.set({ "n", "i", "v" }, "<D-p>", function()
-        require("telescope.builtin").find_files()
-      end)
+			vim.keymap.set({ "n", "i", "v" }, "<D-p>", function()
+				require("telescope.builtin").find_files()
+			end)
 
-      vim.keymap.set({ "n", "i", "v" }, "<D-P>", function()
-        require("telescope.builtin").commands()
-      end)
+			vim.keymap.set({ "n", "i", "v" }, "<D-P>", function()
+				require("telescope.builtin").commands()
+			end)
 
-      vim.keymap.set("n", "<D-F>", function()
-        require("telescope.builtin").live_grep()
-      end)
+			vim.keymap.set("n", "<D-F>", function()
+				require("telescope.builtin").live_grep()
+			end)
 
-      vim.keymap.set("n", "<D-t>", function()
-        require("telescope.builtin").lsp_workspace_symbols()
-      end)
-    end,
-  },
-  {
-    "debugloop/telescope-undo.nvim",
-    dependencies = { -- note how they're inverted to above example
-      {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-      },
-    },
-    opts = {
-      -- don't use `defaults = { }` here, do this in the main telescope spec
-      extensions = {
-        undo = {
-          -- telescope-undo.nvim config, see below
-        },
-        -- no other extensions here, they can have their own spec too
-      },
-    },
-    config = function(_, opts)
-      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-      -- defaults, as well as each extension).
-      require("telescope").setup(opts)
-      require("telescope").load_extension("undo")
+			vim.keymap.set("n", "<D-t>", function()
+				require("telescope.builtin").lsp_workspace_symbols()
+			end)
+		end,
+	},
+	{
+		"debugloop/telescope-undo.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope.nvim",
+				dependencies = { "nvim-lua/plenary.nvim" },
+			},
+		},
+		opts = {
+			extensions = {
+				undo = {},
+			},
+		},
+		config = function(_, opts)
+			require("telescope").setup(opts)
+			require("telescope").load_extension("undo")
 
-      vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", {
-        desc = "undo history",
-      })
-    end,
-  },
+			vim.keymap.set("n", "<leader>u", function()
+				require("telescope").extensions.undo.undo({
+					initial_mode = "normal",
+					mappings = {
+						n = {
+							["l"] = require("telescope-undo.actions").restore,
+						},
+					},
+				})
+			end, {
+				desc = "undo history",
+			})
+		end,
+	},
 }
