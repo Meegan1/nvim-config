52a9f92996d00d5d54520385767b2d9e29966392
Meegan1 <meegan118@gmail.com>
2024-09-20 14:26:31 +0100

    chore: move neo-tree keymaps into plugin config

--- lua/plugins/local/neo-tree.lua
+++ lua/plugins/local/neo-tree.lua
@@ -86,5 +86,21 @@ return {
         },
       },
     },
+
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
+
+      local neo_tree = require("neo-tree")
+
+      -- bind <C-b> to toggle neotree
+      vim.keymap.set({ "n", "i", "v" }, "<D-b>", function()
+        vim.cmd("Neotree toggle")
+      end)
+
+      -- bind <C-A-M-f> to find file in neotree
+      vim.keymap.set({ "n", "i", "v" }, "<C-f>", function()
+        vim.cmd("Neotree reveal")
+      end)
+    end,
   },
 }
