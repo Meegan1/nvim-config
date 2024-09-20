7099decf375ff08d38933c88ca28252bc06bcf57
Meegan1 <meegan118@gmail.com>
2024-09-20 10:04:37 +0100

    fix: capture wezterm/tmux focus key and stop input

---
+++ lua/config/wezterm-focus/local.lua
@@ -0,0 +1,27 @@
+vim.keymap.set(
+  {
+    "n",
+    "i",
+    "x",
+    "t",
+  },
+  "<Char-1105920>",
+  function() end,
+  {
+    desc = "Capture focus out key",
+  }
+)
+
+vim.keymap.set(
+  {
+    "n",
+    "i",
+    "x",
+    "t",
+  },
+  "<Char-1105921>",
+  function() end,
+  {
+    desc = "Capture focus in key",
+  }
+)
