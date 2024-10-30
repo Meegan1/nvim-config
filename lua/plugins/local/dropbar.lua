d4062915c26df74bc10d42751db50f708ec07814
Meegan1 <meegan118@gmail.com>
2024-10-30 14:18:01 +0000

    feat: add <leader>db keybind for dropbar

--- lua/plugins/local/dropbar.lua
+++ lua/plugins/local/dropbar.lua
@@ -5,16 +5,43 @@ return {
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
     },
-    opts = {
-      bar = {
-        sources = function(buf, _)
-          local sources = require("dropbar.sources")
+    config = function(_, config)
+      local utils = require("dropbar.utils")
+      local api = require("dropbar.api")
 
-          return {
-            sources.path,
-          }
-        end,
-      },
-    },
+      require("dropbar").setup({
+
+        bar = {
+          sources = function(buf, _)
+            local sources = require("dropbar.sources")
+
+            return {
+              sources.path,
+            }
+          end,
+        },
+        menu = {
+          preview = false,
+          keymaps = {
+            ["l"] = function()
+              local menu = utils.menu.get_current()
+              if not menu then
+                return
+              end
+              local cursor = vim.api.nvim_win_get_cursor(menu.win)
+              local component = menu.entries[cursor[1]]:first_clickable(cursor[2])
+              if component then
+                menu:click_on(component, nil, 1, "l")
+              end
+            end,
+            ["h"] = "<C-w>q",
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>db", function()
+        api.pick()
+      end, { noremap = true, silent = true })
+    end,
   },
 }
