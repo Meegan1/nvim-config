fbb992b2745062fd4d3c4a8197502a2a01b03b78
Meegan1 <meegan118@gmail.com>
2024-11-09 18:47:10 +0000

    feat: add diffview plugin

--- lua/plugins/local/neogit.lua
+++ lua/plugins/local/neogit.lua
@@ -1,6 +1,12 @@
 return {
   {
     "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+
+      "nvim-telescope/telescope.nvim",
+    },
     opts = {
       disable_insert_on_commit = true,
       graph_style = "unicode",
