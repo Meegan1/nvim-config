4028c67efc3c4d5676c370a8589c574844dd2538
Meegan1 <meegan118@gmail.com>
2024-11-12 02:55:06 +0000

    feat: added mini.indentscope to replace scoped indent line in indent-blankline

--- lua/plugins/local/indent-blankline.lua
+++ lua/plugins/local/indent-blankline.lua
@@ -1,12 +1,30 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  ---@module "ibl"
-  ---@type ibl.config
-  opts = {
-    scope = {
-      show_start = false,
-      show_end = false,
+  {
+    "echasnovski/mini.indentscope",
+    opts = function()
+      vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#3b4261" })
+
+      return {
+        draw = {
+          delay = 0,
+          animation = require("mini.indentscope").gen_animation.none(),
+        },
+        options = {
+          try_as_border = true,
+        },
+        symbol = "▎",
+      }
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+      scope = {
+        enabled = false,
+      },
     },
   },
 }
