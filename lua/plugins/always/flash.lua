0453700e793e5d97f8acf53d64d64c9df9f45832
Meegan1 <meegan118@gmail.com>
2024-11-20 11:54:37 +0000

    feat(flash): enable smart case for flash jumps

--- lua/plugins/always/flash.lua
+++ lua/plugins/always/flash.lua
@@ -1,51 +1,68 @@
+local useSmartCase = function(callback)
+	local ignorecase = vim.go.ignorecase
+	local smartcase = vim.go.smartcase
+
+	vim.go.ignorecase = true
+	vim.go.smartcase = true
+
+	callback()
+
+	vim.go.ignorecase = ignorecase
+	vim.go.smartcase = smartcase
+end
+
 return {
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    config = function(_, opts)
-      require("flash").setup(opts)
-    end,
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "o" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-        desc = "Remote Flash",
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function()
-          require("flash").treesitter_search()
-        end,
-        desc = "Treesitter Search",
-      },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function()
-          require("flash").toggle()
-        end,
-        desc = "Toggle Flash Search",
-      },
-    },
-  },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("flash").setup(opts)
+		end,
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					useSmartCase(function()
+						require("flash").jump()
+					end)
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					useSmartCase(function()
+						require("flash").remote()
+					end)
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
 }
