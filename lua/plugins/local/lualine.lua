d6e3077692933b029d021af0b2b6b478cb35c85a
Meegan1 <meegan118@gmail.com>
2024-12-05 11:06:38 +0000

    feat: add pill to statusline to show if recording macro

--- lua/plugins/local/lualine.lua
+++ lua/plugins/local/lualine.lua
@@ -1,33 +1,50 @@
 return {
-  {
-    'AndreM222/copilot-lualine',
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = {
-      'AndreM222/copilot-lualine',
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = true,
-    opts = {
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff',
-          {
-            'diagnostics',
-            sources = { "nvim_diagnostic" },
-            symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
-          }
-        },
-        lualine_c = {
-          function()
-            return require('auto-session.lib').current_session_name(true)
-          end
-        },
-        lualine_x = { 'copilot', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location', 'searchcount' },
-      },
-    }
-  },
+	{
+		"AndreM222/copilot-lualine",
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"AndreM222/copilot-lualine",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = true,
+		opts = function()
+			return {
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = {
+						"branch",
+						"diff",
+						{
+							"diagnostics",
+							sources = { "nvim_diagnostic" },
+							symbols = { error = " ", warn = " ", info = " ", hint = " " },
+						},
+					},
+					lualine_c = {
+						function()
+							return require("auto-session.lib").current_session_name(true)
+						end,
+					},
+					lualine_x = {
+						-- Macro recording
+						{
+							function()
+								return "recording @" .. vim.fn.reg_recording()
+							end,
+							cond = function()
+								return vim.fn.reg_recording() ~= ""
+							end,
+							color = { fg = "#f8f8f2", bg = "#282a36" },
+						},
+						"copilot",
+						"filetype",
+					},
+					lualine_y = { "progress" },
+					lualine_z = { "location", "searchcount" },
+				},
+			}
+		end,
+	},
 }
