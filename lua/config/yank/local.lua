95725d7e00ce64925468193e3dd13d246381434b
Meegan1 <meegan118@gmail.com>
2025-01-06 13:30:08 +0000

    fix: update deprecated callback

--- lua/config/yank/local.lua
+++ lua/config/yank/local.lua
@@ -2,30 +2,30 @@
 local preserve_cursor = {}
 
 preserve_cursor.state = {
-  cursor = { 0, 0 },
+	cursor = { 0, 0 },
 }
 vim.api.nvim_create_autocmd({ "VimEnter", "CursorMoved" }, {
-  group = vim.api.nvim_create_augroup("NoMoveYank", { clear = true }),
-  callback = function()
-    preserve_cursor.state.cursor = vim.api.nvim_win_get_cursor(0)
-  end,
+	group = vim.api.nvim_create_augroup("NoMoveYank", { clear = true }),
+	callback = function()
+		preserve_cursor.state.cursor = vim.api.nvim_win_get_cursor(0)
+	end,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = "NoMoveYank",
-  callback = function()
-    if vim.v.event.operator == "y" and preserve_cursor.state.cursor then
-      vim.api.nvim_win_set_cursor(0, preserve_cursor.state.cursor)
-    end
-  end,
+	group = "NoMoveYank",
+	callback = function()
+		if vim.v.event.operator == "y" and preserve_cursor.state.cursor then
+			vim.api.nvim_win_set_cursor(0, preserve_cursor.state.cursor)
+		end
+	end,
 })
 
 -- Highlight yanked text
 vim.api.nvim_set_hl(0, "YankHighlight", { link = "Search", default = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-  callback = function()
-    pcall(vim.highlight.on_yank, { higroup = "YankHighlight", timeout = 200 })
-  end,
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	callback = function()
+		pcall(vim.hl.on_yank, { higroup = "YankHighlight", timeout = 200 })
+	end,
 })
