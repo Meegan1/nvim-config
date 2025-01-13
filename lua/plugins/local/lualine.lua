dc5b3d48bfb1dc0356a355fbabedced97e948c74
Meegan1 <meegan118@gmail.com>
2025-01-13 10:53:42 +0000

    feat: add code companion lualine component

--- lua/plugins/local/lualine.lua
+++ lua/plugins/local/lualine.lua
@@ -10,6 +10,54 @@ return {
 		},
 		config = true,
 		opts = function()
+			local CodeCompanionStatus = require("lualine.component"):extend()
+
+			CodeCompanionStatus.processing = false
+			CodeCompanionStatus.spinner_index = 1
+
+			local spinner_symbols = {
+				"⠋",
+				"⠙",
+				"⠹",
+				"⠸",
+				"⠼",
+				"⠴",
+				"⠦",
+				"⠧",
+				"⠇",
+				"⠏",
+			}
+			local spinner_symbols_len = 10
+
+			-- Initializer
+			function CodeCompanionStatus:init(options)
+				CodeCompanionStatus.super.init(self, options)
+
+				local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+				vim.api.nvim_create_autocmd({ "User" }, {
+					pattern = "CodeCompanionRequest*",
+					group = group,
+					callback = function(request)
+						if request.match == "CodeCompanionRequestStarted" then
+							self.processing = true
+						elseif request.match == "CodeCompanionRequestFinished" then
+							self.processing = false
+						end
+					end,
+				})
+			end
+
+			-- Function that runs every time statusline is updated
+			function CodeCompanionStatus:update_status()
+				if self.processing then
+					self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+					return spinner_symbols[self.spinner_index]
+				else
+					return nil
+				end
+			end
+
 			return {
 				sections = {
 					lualine_a = { "mode" },
@@ -39,6 +87,9 @@ return {
 							color = { fg = "#f8f8f2", bg = "#282a36" },
 						},
 						"copilot",
+						{
+							CodeCompanionStatus,
+						},
 						"filetype",
 					},
 					lualine_y = { "progress" },
