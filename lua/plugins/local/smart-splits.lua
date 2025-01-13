b1877b4531fa883ec571249bef107735d1437f74
Meegan1 <meegan118@gmail.com>
2025-01-13 14:18:10 +0000

    feat: enable smart-splits in terminal mode

--- lua/plugins/local/smart-splits.lua
+++ lua/plugins/local/smart-splits.lua
@@ -1,66 +1,74 @@
 return {
-  {
-    "mrjones2014/smart-splits.nvim",
-    keys = {
-      {
-        "<C-w>h",
-        function()
-          require("smart-splits").move_cursor_left()
-        end,
-        desc = "Move left",
-      },
-      {
-        "<C-w>l",
-        function()
-          require("smart-splits").move_cursor_right()
-        end,
-        desc = "Move right",
-      },
-      {
-        "<C-w>k",
-        function()
-          require("smart-splits").move_cursor_up()
-        end,
-        desc = "Move up",
-      },
-      {
-        "<C-w>j",
-        function()
-          require("smart-splits").move_cursor_down()
-        end,
-        desc = "Move down",
-      },
-      {
-        "<A-S-w>h",
-        function()
-          require("smart-splits").swap_buf_left()
-        end,
-        desc = "Swap buffer left",
-      },
-      {
-        "<A-S-w>l",
-        function()
-          require("smart-splits").swap_buf_right()
-        end,
-        desc = "Swap buffer right",
-      },
-      {
-        "<A-S-w>k",
-        function()
-          require("smart-splits").swap_buf_up()
-        end,
-        desc = "Swap buffer up",
-      },
-      {
-        "<A-S-w>j",
-        function()
-          require("smart-splits").swap_buf_down()
-        end,
-        desc = "Swap buffer down",
-      },
-    },
-    opts = {
-      at_edge = "stop",
-    },
-  },
+	{
+		"mrjones2014/smart-splits.nvim",
+		keys = {
+			{
+				"<C-w>h",
+				function()
+					require("smart-splits").move_cursor_left()
+				end,
+				mode = { "n", "t" },
+				desc = "Move left",
+			},
+			{
+				"<C-w>l",
+				function()
+					require("smart-splits").move_cursor_right()
+				end,
+				mode = { "n", "t" },
+				desc = "Move right",
+			},
+			{
+				"<C-w>k",
+				function()
+					require("smart-splits").move_cursor_up()
+				end,
+				mode = { "n", "t" },
+				desc = "Move up",
+			},
+			{
+				"<C-w>j",
+				function()
+					require("smart-splits").move_cursor_down()
+				end,
+				mode = { "n", "t" },
+				desc = "Move down",
+			},
+			{
+				"<A-S-w>h",
+				function()
+					require("smart-splits").swap_buf_left()
+				end,
+				mode = { "n", "t" },
+				desc = "Swap buffer left",
+			},
+			{
+				"<A-S-w>l",
+				function()
+					require("smart-splits").swap_buf_right()
+				end,
+				mode = { "n", "t" },
+				desc = "Swap buffer right",
+			},
+			{
+				"<A-S-w>k",
+				function()
+					require("smart-splits").swap_buf_up()
+				end,
+				mode = { "n", "t" },
+				desc = "Swap buffer up",
+			},
+			{
+				"<A-S-w>j",
+				function()
+					require("smart-splits").swap_buf_down()
+				end,
+				mode = { "n", "t" },
+				desc = "Swap buffer down",
+			},
+		},
+		opts = {
+			at_edge = "stop",
+		},
+	},
 }
