07e14544a63d96c1370a385cdf9156f64d405759
Meegan1 <meegan118@gmail.com>
2025-01-15 04:37:23 +0000

    feat: replace nvim-spectre with grug-far.nvim

---
+++ lua/plugins/local/grug-far.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"MagicDuck/grug-far.nvim",
+		opts = {
+			headerMaxWidth = 80,
+			startInInsertMode = false,
+
+			prefills = {
+				flags = "-u --hidden --glob=!{.git,node_modules,.nx,.next,dist,coverage}",
+			},
+		},
+		cmd = "GrugFar",
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					local grug = require("grug-far")
+					grug.open({
+						transient = true,
+						instanceName = "default",
+					})
+				end,
+				mode = { "n", "v" },
+				desc = "Search and Replace",
+			},
+		},
+
+		config = function(_, opts)
+			require("grug-far").setup(opts)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "grug-far",
+				callback = function()
+					vim.keymap.set("n", "q", function()
+						require("grug-far").close_instance("default")
+					end, { buffer = true, silent = true, desc = "Close grug-far window" })
+				end,
+			})
+		end,
+	},
+}
