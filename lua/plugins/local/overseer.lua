87fa5aab09459b525b9d551bbeaed5f35c78da98
Meegan1 <meegan118@gmail.com>
2025-02-14 02:06:14 +0000

    feat: add keymaps for overseer

--- lua/plugins/local/overseer.lua
+++ lua/plugins/local/overseer.lua
@@ -2,5 +2,18 @@ return {
 	{
 		"stevearc/overseer.nvim",
 		opts = {},
+		config = function(_, opts)
+			require("overseer").setup(opts)
+
+			-- Add keymap <leader>oo to toggle the overseer window
+			vim.keymap.set("n", "<leader>oo", function()
+				require("overseer").toggle()
+			end, { noremap = true, silent = true, desc = "Open overseer" })
+
+			-- Add keymap <leader>or to run a command in the overseer window
+			vim.keymap.set("n", "<leader>or", function()
+				vim.cmd("OverseerRun")
+			end, { noremap = true, silent = true, desc = "Run command in overseer" })
+		end,
 	},
 }
