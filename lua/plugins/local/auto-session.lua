05135e04df47e81396923292f9674438d90bc202
Meegan1 <meegan118@gmail.com>
2025-02-21 10:59:42 +0000

    feat: add new keybinds for overseer + auto-session integration

--- lua/plugins/local/auto-session.lua
+++ lua/plugins/local/auto-session.lua
@@ -33,7 +33,13 @@ return {
 				return true
 			end)()
 
-			function close_all_floating_wins()
+			-- Convert the cwd to a simple file name
+			local function get_cwd_as_name()
+				local dir = vim.fn.getcwd(0)
+				return dir:gsub("[^A-Za-z0-9]", "_")
+			end
+
+			local function close_all_floating_wins()
 				for _, win in ipairs(vim.api.nvim_list_wins()) do
 					local config = vim.api.nvim_win_get_config(win)
 					if config.relative ~= "" then
@@ -41,6 +47,8 @@ return {
 					end
 				end
 			end
+
+			local overseer = require("overseer")
 
 			require("auto-session").setup({
 				auto_restore_enabled = auto_restore_enabled,
@@ -60,9 +68,33 @@ return {
 					function()
 						vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" })
 					end,
+					function()
+						require("overseer.window").close()
+
+						overseer.save_task_bundle(
+							get_cwd_as_name(),
+							-- Passing nil will use config.opts.save_task_opts. You can call list_tasks() explicitly and
+							-- pass in the results if you want to save specific tasks.
+							nil,
+							{ on_conflict = "overwrite" } -- Overwrite existing bundle, if any
+						)
+					end,
 				},
+
+				pre_restore_cmds = {
+					-- Get rid of all previous tasks when restoring a session
+					function()
+						for _, task in ipairs(overseer.list_tasks({})) do
+							task:dispose(true)
+						end
+					end,
+				},
+
 				post_restore_cmds = {
 					open_neo_tree,
+					function()
+						overseer.load_task_bundle(get_cwd_as_name(), { ignore_missing = true })
+					end,
 					"stopinsert", -- Stop insert mode after restoring session
 				},
 
--- lua/plugins/local/overseer.lua
+++ lua/plugins/local/overseer.lua
@@ -5,6 +5,35 @@ return {
 		config = function(_, opts)
 			require("overseer").setup(opts)
 
+			local get_sidebar = function()
+				return require("overseer.task_list.sidebar").get_or_create()
+			end
+
+			local jump_task = function(num)
+				local sidebar = get_sidebar()
+				local focused_task = sidebar.focused_task_id
+				local task_lines = sidebar.task_lines
+
+				-- loop over the task lines to find the focused task
+				for i, line in ipairs(task_lines) do
+					if line[2].id == focused_task then
+						local target_index
+
+						-- calculate target index with wrap-around
+						if i + num > #task_lines then
+							target_index = 1
+						elseif i + num < 1 then
+							target_index = #task_lines
+						else
+							target_index = i + num
+						end
+
+						sidebar:focus_task_id(task_lines[target_index][2].id)
+						break
+					end
+				end
+			end
+
 			-- Add keymap <leader>oo to toggle the overseer window
 			vim.keymap.set("n", "<leader>oo", function()
 				require("overseer").toggle()
@@ -14,6 +43,27 @@ return {
 			vim.keymap.set("n", "<leader>or", function()
 				vim.cmd("OverseerRun")
 			end, { noremap = true, silent = true, desc = "Run command in overseer" })
+
+			-- Jump to the next task
+			vim.keymap.set("n", "<leader>on", function()
+				jump_task(1)
+			end, { noremap = true, silent = true, desc = "Jump to the next task" })
+			vim.keymap.set("n", "]o", function()
+				jump_task(1)
+			end, { noremap = true, silent = true, desc = "Jump to the next task" })
+
+			-- Jump to the previous task
+			vim.keymap.set("n", "<leader>op", function()
+				jump_task(-1)
+			end, { noremap = true, silent = true, desc = "Jump to the previous task" })
+			vim.keymap.set("n", "[o", function()
+				jump_task(-1)
+			end, { noremap = true, silent = true, desc = "Jump to the previous task" })
+
+			-- Restart the task
+			vim.keymap.set("n", "<leader>oR", function()
+				get_sidebar():run_action("restart")
+			end, { noremap = true, silent = true, desc = "Restart the task" })
 		end,
 	},
 }
