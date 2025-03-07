ad050687c6054028a24be36b5d244e29a97a994c
Meegan1 <meegan118@gmail.com>
2025-03-07 00:56:12 +0000

    feat: use j/k for moving between wrapped lines

--- lua/config/keymaps/local.lua
+++ lua/config/keymaps/local.lua
@@ -50,3 +50,11 @@ vim.keymap.set("t", "<S-Esc>", "<C-\\><C-n>")
 vim.keymap.set("n", "<leader>rn", function()
 	vim.lsp.buf.rename()
 end)
+
+-- Move visual/wrapped lines with j/k whilst preserving v.count expressions
+vim.keymap.set({ "n", "x" }, "j", function()
+	return vim.v.count > 0 and "j" or "gj"
+end, { expr = true })
+vim.keymap.set({ "n", "x" }, "k", function()
+	return vim.v.count > 0 and "k" or "gk"
+end, { expr = true })
