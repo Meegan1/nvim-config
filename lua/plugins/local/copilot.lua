6df489e27d0b09ae397015f3e2255af74c159097
Meegan1 <meegan118@gmail.com>
2025-04-12 19:32:09 +0100

    feat: update copilot.lua to latest version, fixes issue with lualine

--- lua/plugins/local/copilot.lua
+++ lua/plugins/local/copilot.lua
@@ -2,12 +2,10 @@ return {
 	{
 		"zbirenbaum/copilot.lua",
 
-		-- https://github.com/zbirenbaum/copilot.lua/issues/365#issuecomment-2708887411
-		branch = "create-pull-request/update-copilot-dist",
-
 		cmd = "Copilot",
 		event = "InsertEnter",
 		opts = {
+			copilot_model = "gpt-4o-copilot",
 			suggestion = {
 				auto_trigger = true,
 				keymap = {
@@ -26,36 +24,7 @@ return {
 			},
 		},
 		config = function(_, opts)
-			local new_opts = vim.tbl_extend("force", opts, {
-				server_opts_overrides = {
-					cmd = {
-						"node",
-						vim.api.nvim_get_runtime_file("copilot/dist/language-server.js", false)[1],
-						"--stdio",
-					},
-					init_options = {
-						copilotIntegrationId = "vscode-chat",
-					},
-				},
-			})
-
-			require("copilot").setup(new_opts)
-
-			local util = require("copilot.util")
-			local orig_get_editor_configuration = util.get_editor_configuration
-
-			---@diagnostic disable-next-line: duplicate-set-field
-			util.get_editor_configuration = function()
-				local config = orig_get_editor_configuration()
-
-				return vim.tbl_extend("force", config, {
-					github = {
-						copilot = {
-							selectedCompletionModel = "gpt-4o-copilot",
-						},
-					},
-				})
-			end
+			require("copilot").setup(opts)
 		end,
 	},
 }
