6059618e891d5a0cb8fe279cfe384fa9b9268af9
Meegan1 <meegan118@gmail.com>
2025-05-09 11:36:23 +0100

    feat: update codecompanion default copilot model to gpt-4.1

--- lua/plugins/local/codecompanion.lua
+++ lua/plugins/local/codecompanion.lua
@@ -30,7 +30,7 @@ return {
 		local copilot_adapter = require("codecompanion.adapters").extend("copilot", {
 			schema = {
 				model = {
-					default = "gpt-4o",
+					default = "gpt-4.1",
 				},
 			},
 		})
@@ -41,7 +41,7 @@ return {
 					return require("codecompanion.adapters").extend("copilot", {
 						schema = {
 							model = {
-								default = "gpt-4o",
+								default = "gpt-4.1",
 							},
 						},
 					})
