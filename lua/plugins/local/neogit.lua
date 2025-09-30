ee2246503cb8c03c3ff4168b44f16cf9d5d7fcaf
Meegan1 <jake@meegan.me>
2025-09-30 17:00:59 +0100

    feat: remove neogit keymaps and use fugitive instead

--- lua/plugins/local/neogit.lua
+++ lua/plugins/local/neogit.lua
@@ -1,24 +1,20 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
 
-      "nvim-telescope/telescope.nvim",
-    },
-    opts = {
-      disable_insert_on_commit = true,
-      graph_style = "unicode",
-      process_spinner = false,
-    },
-    config = function(_, config)
-      local neogit = require("neogit")
-      neogit.setup(config)
-
-      vim.keymap.set({ "n" }, "<leader>gs", function()
-        neogit.open()
-      end, { noremap = true, silent = true })
-    end,
-  },
+			"nvim-telescope/telescope.nvim",
+		},
+		opts = {
+			disable_insert_on_commit = true,
+			graph_style = "unicode",
+			process_spinner = false,
+		},
+		config = function(_, config)
+			local neogit = require("neogit")
+			neogit.setup(config)
+		end,
+	},
 }
