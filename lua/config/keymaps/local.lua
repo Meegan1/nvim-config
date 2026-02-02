bcf12a418bb4041237e0ccc938fac34093553dfe
Meegan1 <jake@meegan.me>
2026-02-02 01:12:12 +0000

    fix: remove outdated buffer keymaps

--- lua/config/keymaps/local.lua
+++ lua/config/keymaps/local.lua
@@ -1,11 +1,3 @@
--- map BufferNext and BufferPrevious to gt and gT
-vim.keymap.set("n", "gt", function()
-	vim.cmd("BufferNext")
-end)
-vim.keymap.set("n", "gT", function()
-	vim.cmd("BufferPrevious")
-end)
-
 vim.keymap.set({
 	"i",
 }, "<Char-1106366>", "<C-d>")
@@ -16,31 +8,6 @@ vim.keymap.set("i", "<S-Tab>", "<C-d>")
 -- save on cmd + s
 vim.keymap.set({ "n", "i", "v" }, "<C-s>", function()
 	vim.cmd("w")
-end)
-
-vim.keymap.set({ "n", "i", "v" }, "<D-w>", function()
-	local Snacks = require("snacks")
-	Snacks.bufdelete()
-end)
-
--- reopen last closed buffer on cmd + shift + t
-vim.keymap.set({ "n", "i", "v" }, "<C-S-T>", function()
-	vim.cmd("BufferRestore")
-end)
-
--- create a new buffer on cmd + n
-vim.keymap.set({ "n" }, "<C-S-n>", function()
-	vim.cmd("enew")
-end)
-
--- undo on cmd + z
-vim.keymap.set({ "n", "i" }, "<C-z>", function()
-	vim.cmd("u")
-end)
-
--- redo on cmd + shift + z
-vim.keymap.set({ "n", "i" }, "<C-S-Z>", function()
-	vim.cmd("redo")
 end)
 
 -- escape from terminal mode with shift + escape
